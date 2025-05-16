d52e34f0bf7f5b9fab743b7d25fbd91457407b06
wilmarRamirez(oficina) <wilmar.0614@gmail.com>
2025-05-16 16:01:56 -0500

    feat(app): se organiza codigo

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
-import { botChallenger } from '#server/challenger/bot';
-import {botHaceb} from '#interface/haceb/bot'
+import { botChallenger } from "#server/challenger/bot";
+import { botHaceb } from "#interface/haceb/bot";
 
-(async() =>{
-    
-    await botChallenger()
-        .then(() => {
-            console.log("✅ Proceso de extracción de datos finalizado.");
-        })
-        .catch((error) => {
-            console.error("❌ Error durante la extracción de datos:", error);
-        }); 
+(async () => {
+  await botChallenger()
+    .then(() => {
+      console.log("✅ Proceso de extracción de datos finalizado.");
+    })
+    .catch((error) => {
+      console.error("❌ Error durante la extracción de datos:", error);
+    });
 
-    await botHaceb()
-        .then(() => {
-            console.log("✅ Proceso de extracción de datos finalizado.");
-        })
-        .catch((error) => {
-            console.error("❌ Error durante la extracción de datos:", error);
-        });
-})()
+  await botHaceb()
+    .then(() => {
+      console.log("✅ Proceso de extracción de datos finalizado.");
+    })
+    .catch((error) => {
+      console.error("❌ Error durante la extracción de datos:", error);
+    });
+})();
